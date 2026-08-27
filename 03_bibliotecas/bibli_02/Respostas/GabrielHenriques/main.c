62246b1a95d84f194e546873d4fa32298fd76e59
Gabriel Henriques <gabrielhenriques1603@gmail.com>
2026-08-27 11:05:14 -0300

    começando bibli_02, muitos problemas pela frente

---
+++ 03_bibliotecas/bibli_02/Respostas/GabrielHenriques/main.c
@@ -0,0 +1,14 @@
+#include "matrix_utils.h"
+#include <stdio.h>
+
+int main() {
+    int rows1, rows2, cols1, cols2;
+    int matrix1[rows1][cols1], matrix2[rows2][cols2];
+
+    void matrix_read(rows1, cols1, matrix1);
+    void matrix_read(rows2, cols2, matrix2);
+
+    void matrix_print(rows1, cols1, matrix1);
+    void matrix_print(rows2, cols2, matrix2);
+
+}
